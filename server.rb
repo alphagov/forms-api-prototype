f1ccc2a1f49d33e612740ba3a792ce357b30f703
Dan Burnley <dan@danops.uk>
2022-01-17 17:18:14 +0000

    Store forms in the database rather than files

--- server.rb
+++ server.rb
@@ -1,64 +1,96 @@
 require "sinatra"
 require "json"
 require "pry"
+require_relative "./db/database"
 
-before do
-  content_type :json
-end
-
-post "/publish" do
-  user = request.env['HTTP_X_API_KEY']
-  request_body = request.body.read
-  request = JSON.parse(request_body)
-
-  id = request['id']
-  config = {}
-  config = request['configuration'] if request['configuration']
-  filename = id
-  filename = "#{id}_#{user}" if user
-
-  File.write("./forms/#{filename}", JSON.dump(config))
-
-  config.to_json
-end
-
-get "/published" do
-  user = request.env['HTTP_X_API_KEY']
-
-  files = []
-  forms = []
-
-  if user
-    forms = Dir.entries("./forms").select { |filename| File.file?("./forms/#{filename}") && filename.include?(user) }
-  else
-    forms = Dir.entries("./forms").select { |filename| File.file?("./forms/#{filename}") }
+class Server < Sinatra::Base
+  before do
+    content_type :json
+    @database = Database.new.connect
   end
-  forms.each do |form|
-    File.open("./forms/#{form}") do |f|
-      files << {
-        "Key": form,
-        "DisplayName": form,
+
+  after do
+    @database.disconnect
+  end
+
+  post "/publish" do
+    user = request.env['HTTP_X_API_KEY']
+
+    request_body = request.body.read
+    request = JSON.parse(request_body)
+
+    id = request['id']
+    config = {}
+    config = request['configuration'] if request['configuration']
+
+    @database[:forms].insert(
+      username: user,
+      key: id,
+      display_name: id,
+      form: Sequel.pg_json(config)
+    )
+
+    config.to_json
+  end
+
+  get "/published" do
+    user = request.env['HTTP_X_API_KEY']
+    forms = []
+
+    forms_for_user(user).each do |form|
+      forms << {
+        "Key": form[:key],
+        "DisplayName": form[:display_name],
         "FeedbackForm": false
       }
     end
+
+    forms.to_json
   end
 
-  files.to_json
-end
+  get "/published/:id" do
+    user = request.env['HTTP_X_API_KEY']
+    form = @database[:forms].where(username: user, key: params['id']).first
 
-get "/published/:id" do
-  user = request.env['HTTP_X_API_KEY']
-  form_content = {}
-  filename = params['id']
-  filename = "#{params['id']}_#{user}" if user
-
-  File.open("./forms/#{filename}") do |f|
-    file_content = f.read
-    form_content = JSON.parse(file_content)
+    {
+      id: form[:key],
+      values: form[:form]
+    }.to_json
   end
 
-  {
-    id: filename,
-    values: form_content
-  }.to_json
+  get "/seed/:user" do
+    seed_data_for_user(params['user'])
+
+    @database[:forms].where(username: params['user']).select(:key).all.to_json
+  end
+
+  private
+
+  def forms_for_user(user)
+    forms = @database[:forms].where(username: user).all
+
+    if forms.empty?
+      seed_data_for_user(user)
+      return @database[:forms].where(username: user).all
+    end
+
+    forms
+  end
+
+  def seed_data_for_user(user)
+    forms = Dir.entries("./example_forms").select { |filename| File.file?("./example_forms/#{filename}") }
+    forms.map do |filename|
+      File.open("./example_forms/#{filename}") do |f|
+        file_content = f.read
+        if @database[:forms].where(username: user).where(key: filename).all.count == 0
+          @database[:forms].insert(
+            username: user,
+            key: filename,
+            display_name: filename,
+            form: file_content
+          )
+        end
+      end
+    end
+  end
 end
