722c6d0606c1cccbbaaf5fced0c8f9b0a129043a
Tristram Oaten <tris@oat.sh>
2022-02-07 16:37:57 +0000

    Initial rust project

---
+++ rust-api/src/main.rs
@@ -0,0 +1,31 @@
+use poem::{listener::TcpListener, Route, Server};
+use poem_openapi::{param::Query, payload::PlainText, OpenApi, OpenApiService};
+
+struct Api;
+
+#[OpenApi]
+impl Api {
+    #[oai(path = "/test", method = "get")]
+    async fn index(&self, name: Query<Option<String>>) -> PlainText<String> {
+        match name.0 {
+            Some(name) => PlainText(format!("hello, {}!", name)),
+            None => PlainText("hello!".to_string()),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "poem=debug");
+    }
+    tracing_subscriber::fmt::init();
+
+    let api_service = OpenApiService::new(Api, "Poem REST Demo", "0.0.1-alpha")
+        .server("http://localhost:3000/api");
+    let ui = api_service.swagger_ui();
+
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .run(Route::new().nest("/api", api_service).nest("/", ui))
+        .await
+}
