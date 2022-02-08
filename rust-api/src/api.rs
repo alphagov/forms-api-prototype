f5371038fd069c0e4908497075ac5105b1f61cf8
Tristram Oaten <tris@oat.sh>
2022-02-08 12:57:10 +0000

    Moved to native rust, docker postgres

---
+++ rust-api/src/api.rs
@@ -0,0 +1,14 @@
+use poem_openapi::{param::Query, payload::PlainText, OpenApi};
+
+pub struct Api;
+
+#[OpenApi]
+impl Api {
+    #[oai(path = "/test", method = "get")]
+    async fn index(&self, name: Query<Option<String>>) -> PlainText<String> {
+        match name.0 {
+            Some(name) => PlainText(format!("hello, {}!", name)),
+            None => PlainText("hello!".to_string()),
+        }
+    }
+}
--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,31 +1,38 @@
+use color_eyre::Report;
+use color_eyre::eyre::Result;
+
 use poem::{listener::TcpListener, Route, Server};
-use poem_openapi::{param::Query, payload::PlainText, OpenApi, OpenApiService};
+use poem_openapi::{OpenApiService};
+use sqlx::postgres::PgPoolOptions;
 
-struct Api;
+mod api;
 
-#[OpenApi]
-impl Api {
-    #[oai(path = "/test", method = "get")]
-    async fn index(&self, name: Query<Option<String>>) -> PlainText<String> {
-        match name.0 {
-            Some(name) => PlainText(format!("hello, {}!", name)),
-            None => PlainText("hello!".to_string()),
-        }
+fn setup() -> Result<(), Report> {
+    if std::env::var("RUST_LIB_BACKTRACE").is_err() {
+        std::env::set_var("RUST_LIB_BACKTRACE", "0")
     }
+    color_eyre::install()?;
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "poem=debug");
+    };
+    tracing_subscriber::fmt::init();
+
+    Ok(())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "poem=debug");
-    }
-    tracing_subscriber::fmt::init();
+async fn main() -> Result<()> {
+    setup()?;
+    let _pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect("postgres://postgres:password@localhost/postgres").await?;
 
-    let api_service = OpenApiService::new(Api, "Poem REST Demo", "0.0.1-alpha")
-        .server("http://localhost:3000/api");
+    let api_service = OpenApiService::new(api::Api, "Forms API Rust Prototype", "0.0.1-alpha")
+        .server("http://0.0.0.0:3000/api");
     let ui = api_service.swagger_ui();
 
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(Route::new().nest("/api", api_service).nest("/", ui))
-        .await
+        .await?;
+    Ok(())
 }
