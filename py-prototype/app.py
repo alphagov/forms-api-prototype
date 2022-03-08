172f1ce75b18d42d07b2c137e3d26e356d178242
Tristram Oaten <tris@oat.sh>
2022-03-08 11:38:48 +0000

    add /published stub
    Co-authored-by: alice-carr <alice-carr@users.noreply.github.com>

--- py-prototype/app.py
+++ py-prototype/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import Dict
+from typing import Dict, List
 
 from flask import Flask
 from flask_restful import Resource, Api  # type: ignore
@@ -9,12 +9,34 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
-    def get(self) -> Dict[str, str]:
-        return {"hello": "forms"}
+def forms_for_user(user) -> List[Dict[str, str]]:
+    """
+    Query postgres for all the forms for this username
+    """
+    return [{"key": "b", "display_name": "c"}]
 
 
-api.add_resource(HelloWorld, "/")
+class Published(Resource):
+    # def get(self) -> Dict[str, str]:
+    # return {"hello": "forms"}
+
+    def get(self):
+        user = "Alice"
+        forms = []
+
+        for form in forms_for_user(user):
+            forms.append(
+                {
+                    "Key": form["key"],
+                    "DisplayName": form["display_name"],
+                    "FeedbackForm": False,
+                }
+            )
+
+        return forms
+
+
+api.add_resource(Published, "/published")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ py-prototype/server.rb
@@ -0,0 +1,141 @@
+require "sinatra"
+require "json"
+require "pry"
+require 'jwt'
+require_relative "./db/database"
+
+class Server < Sinatra::Base
+  before do
+    content_type :json
+    @database = Database.new.connect
+  end
+
+  after do
+    @database.disconnect
+  end
+
+  post "/publish" do
+    user = authenticated_user
+
+    request_body = request.body.read
+    request = JSON.parse(request_body)
+
+    id = request['id']
+    config = {}
+    config = request['configuration'] if request['configuration']
+
+    if form_exists_for_user?(user, id)
+      @database[:forms].where(
+        username: user,
+        key: id
+      ).update(
+        form: Sequel.pg_json(config)
+      )
+    else
+      @database[:forms].insert(
+        username: user,
+        key: id,
+        display_name: id,
+        form: Sequel.pg_json(config)
+      )
+    end
+
+    config.to_json
+  end
+
+  def form_exists_for_user?(user, key)
+    !@database[:forms].where(username: user, key: key).all.empty?
+  end
+
+  get "/published" do
+    user = authenticated_user
+    forms = []
+
+    forms_for_user(user).each do |form|
+      forms << {
+        "Key": form[:key],
+        "DisplayName": form[:display_name],
+        "FeedbackForm": false
+      }
+    end
+
+    forms.to_json
+  end
+
+  get "/published/:id" do
+    api_key = authenticated_user
+    user = api_key unless api_key.nil? || api_key.empty?
+
+    form = @database[:forms].where(username: user, key: params['id']).first
+
+    if form.nil?
+      response.status = 404
+      return {}.to_json
+    end
+
+    {
+      id: form[:key],
+      values: form[:form]
+    }.to_json
+  end
+
+  get "/seed/:user" do
+    seed_data_for_user(params['user'])
+
+    @database[:forms].where(username: params['user']).select(:key).all.to_json
+  end
+
+  get "/login" do
+    content_type :html
+
+    erb :login
+  end
+
+  post "/login" do
+    content_type :html
+
+    payload = { user: params['name'] }
+    token = JWT.encode payload, nil, 'none'
+
+    redirect "#{ENV['DESIGNER_URL']}/app/auth?token=#{token}"
+  end
+
+  private
+
+  def authenticated_user
+    token = request.env['HTTP_X_API_KEY']
+    begin
+      decoded_token = JWT.decode token, nil, false
+      return decoded_token[0]["user"]
+    rescue
+      return nil
+    end
+  end
+
+  def forms_for_user(user)
+    forms = @database[:forms].where(username: user).all
+
+    if forms.empty?
+      seed_data_for_user(user)
+      return @database[:forms].where(username: user).all
+    end
+
+    forms
+  end
+
+  def seed_data_for_user(user)
+    forms = Dir.entries("./example_forms").select { |filename| File.file?("./example_forms/#{filename}") }
+    forms.map do |filename|
+      File.open("./example_forms/#{filename}") do |f|
+        file_content = f.read
+          @database[:forms].insert(
+            username: user,
+            key: filename,
+            display_name: filename,
+            form: file_content
+          )
+        end
+      end
+    end
+  end
+end
